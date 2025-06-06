32655c9dbde217b5cbf6b4cc794aec5e2b50bc74
Build-1-0-0 <africancontent807@gmail.com>
2025-06-06 17:48:01 +0300

    Update Anime.js

--- server/models/Anime.js
+++ server/models/Anime.js
@@ -1,11 +1,11 @@
-// server/models/Anime.js
 const mongoose = require("mongoose");
 
 const AnimeSchema = new mongoose.Schema({
   title: String,
-  source: String, // "archive" or "webtorrent"
-  url: String, // Archive.org URL or WebTorrent magnet link
+  source: String, // "archive", "webtorrent", "youtube"
+  url: String,
   resolutions: [String],
+  genre: [String],
 });
 
 module.exports = mongoose.model("Anime", AnimeSchema);
